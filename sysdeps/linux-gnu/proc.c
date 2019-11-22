7fcfaa9780aa5c523333dec9fcdfc9122e304f5a
Martin Kampas <martin.kampas@jolla.com>
2019-11-22 09:28:57 +0100

    Revert "Fix deprecated readdir_r usage, fixes jb#44901"
    Reverting prior to merging fixed ustream.
    
    This reverts commit 480e81367a8a688a2b0f1162c538e23ffe0d3fc7.

--- sysdeps/linux-gnu/proc.c
+++ sysdeps/linux-gnu/proc.c
@@ -240,16 +240,14 @@ process_tasks(pid_t pid, pid_t **ret_tasks, size_t *ret_n)
 	size_t alloc = 0;
 
 	while (1) {
-		errno = 0;
-		struct dirent* result = readdir(d);
-		if (!result) {
-			if (errno) {
-				free(tasks);
-				return -1;
-			} else {
-				break;
-			}
+		struct dirent entry;
+		struct dirent *result;
+		if (readdir_r(d, &entry, &result) != 0) {
+			free(tasks);
+			return -1;
 		}
+		if (result == NULL)
+			break;
 		if (result->d_type == DT_DIR && all_digits(result->d_name)) {
 			pid_t npid = atoi(result->d_name);
 			if (n >= alloc) {
